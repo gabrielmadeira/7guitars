f593f2ac9653ca7fb45fc7a2179d182909b272af
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2022-08-12 21:07:19 -0300

    database in mongo

--- database.js
+++ database.js
@@ -1,12 +1,19 @@
-const mongo = require('mongodb');
-var url = "mongodb://localhost:27017/mydb";
-
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database Opened");
-    db.close();
-  });
-  
-
-module.exports = pool.promise();
+const MongoClient = require('mongodb').MongoClient
+ 
+// Note: A production application should not expose database credentials in plain text.
+// For strategies on handling credentials, visit 12factor: https://12factor.net/config.
+const PROD_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
+const MKTG_URI = "mongodb://<dbuser>:<dbpassword>@<host1>:<port1>,<host2>:<port2>/<dbname>?replicaSet=<replicaSetName>"
+ 
+function connect(url) {
+  return MongoClient.connect(url).then(client => client.db())
+}
+ 
+module.exports = async function() {
+  let databases = await Promise.all([connect(PROD_URI), connect(MKTG_URI)])
+ 
+  return {
+    production: databases[0],
+    marketing: databases[1]
+  }
+}
