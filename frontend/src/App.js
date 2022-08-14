eb3a4cb3053166a0b3680441e05b0672796a38b1
JC <jc@JCs-MacBook-Air.local>
2022-08-14 15:49:21 -0300

    Feat: nova tela cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import Test from './Test';
 import Login from './components/login';
+import Register from './components/register';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        < Register />
       </header>
     </div>
   );
