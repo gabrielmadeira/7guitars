e5167d6a4c57146e498548eeda845f8f1156b376
Bruno Guilhem <boguilhem@gmail.com>
2022-08-14 17:34:47 -0300

    feat: adding login and register routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
-import Test from './Test';
-import Login from './components/login';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+// import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
+      <BrowserRouter>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/register" className="nav-link">
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <header className="App-header">
+          <Routes>
+            <Route path="/" exact element={<Login />} />
+            <Route path="/register" exact element={<Register />} />
+          </Routes>
+        </header>
+      </BrowserRouter>
     </div>
   );
 }
