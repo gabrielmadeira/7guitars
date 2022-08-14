2719aba483a8a640ca7cc26dcd07b0ca0a0a945b
JC <jc@JCs-MacBook-Air.local>
2022-08-13 22:17:49 -0300

    Tela login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import Test from './Test';
+import Login from './components/login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Test />
+        <Login />
       </header>
     </div>
   );
