23b06380079c8b0dbf10cfb7a22f3ce6cc017d8f
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2022-08-14 21:12:35 -0300

    json send

--- backend/models/part.js
+++ backend/models/part.js
@@ -1,23 +1,31 @@
 const part = require("../models/partSchema")
 
 module.exports = class Part {
-    static async registerPart(req,res){
+    static async registerPart(req, res) {
         const newPart = new part({
-            id:String,
-            name:req.body.name,
-            quantity:req.body.quantity,
-            section:req.body.type,
-            price:req.body.price,
-            imageURL:req.body.image,
-            dependency:req.body.dependency
-    
-       })
-       await newPart.save()
-       res.send(newPart)
+            name: req.body.name,
+            quantity: req.body.quantity,
+            section: req.body.section,
+            price: req.body.price,
+            description: req.body.description
+        })
+        await newPart.save()
+        res.send(newPart)
     }
-    static async getPartofType(req,res){
-        const findUser = await part.find({ type: req.query.type})
-        res.send(findUser)
+    static async getPartofType(req, res) {
+        await part.find({ type: req.query.type }).then((parts) => {
+            console.log(parts[0].section)
+            var returnJson = " {Nome:" + parts[0].section + ", Variacoes:[";
+            parts.forEach(part => {
+                returnJson += "{nome:" + part.name + ","
+                returnJson += "preco:" + part.price + ","
+                returnJson += "quantidade:"+ part.quantity+","
+                returnJson += "descricao:" + part.description + ",},"
+            });
+            returnJson += "],}"
+            res.send(returnJson)
+        }
+        )
     }
 
 }
--- backend/models/partSchema.js
+++ backend/models/partSchema.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema = mongoose.Schema({
-    id:String,
     name:String,
     quantity:Number, 
     section:String,
     price:String,
-    imageURL:String,
-    dependency:[Number]
+    description:String
 })
 module.exports = mongoose.model("Part",schema)
