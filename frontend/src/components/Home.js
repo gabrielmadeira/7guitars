18ebdf5a21a320ae7171d6d631064b165bcc2dcc
Joao Criscolo <joaocesarcriscolo@gmail.com>
2022-09-29 14:33:55 -0300

    ops tava na main foi mal

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+
+function Home() {
+  return (
+    <div className="homeContainer">
+      <h1> NAVBAR AQUI</h1>
+      <div className="img"> </div>
+    </div>
+  );
+}
+
+export default Home;
