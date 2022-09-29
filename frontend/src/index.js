e7fe698cf77251683129d97176705c34de40bb80
Bruno Guilhem <boguilhem@gmail.com>
2022-09-28 21:05:20 -0300

    feat: install chakra-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ColorModeScript, ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
